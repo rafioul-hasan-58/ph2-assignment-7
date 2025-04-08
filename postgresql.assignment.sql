1455de3940abd4ffba71da0461bdc26e70fe489f
Rafioul Hasan <rafioulhasan2@gmail.com>
2025-04-08 13:00:14 +0600

    short hand added

--- postgresql.assignment.sql
+++ postgresql.assignment.sql
@@ -46,41 +46,33 @@ INSERT INTO orders (customer_id,book_id,quantity) VALUES
 -- solving problems
 
 -- problem:1️⃣ (find the books that are out of stock)
-
 SELECT * FROM books WHERE stock=0;
 
 -- problem:2️⃣ (find the most expensive book)
-
 SELECT max(price) as expensive_book FROM books
 
 -- problem:3️⃣ (find the total number of books ordered by each customer)
-
 SELECT name,count(*) as total_orders FROM orders
 JOIN customers on orders.customer_id=customers.id 
 GROUP BY name
 
 -- problem:4️⃣ (calculate the total revenue sold from all orders)
-
 SELECT SUM(o.quantity*b.price) as total_revenue FROM orders as o
 JOIN books as b ON o.book_id=b.id
 
 
 -- problem:5️⃣ (who ordered more than 1 book)
-
-SELECT customers.name,count(*) as orders_count FROM orders 
-JOIN customers on orders.customer_id=customers.id 
-GROUP BY customers.name
+SELECT c.name,count(o.id) as orders_count FROM orders as o
+JOIN customers as c on o.customer_id=c.id 
+GROUP BY c.name
 
 -- problem:6️⃣ (find the avg price of books)
-
-SELECT AVG(price) as avg_book_price FROM books
+SELECT ROUND(AVG(price),2) as avg_book_price FROM books
 
 -- problem:7️⃣ (increase book price 10% for all books published before 2000)
-
-UPDATE books SET price=price*1.1 WHERE published_year<2000;
+UPDATE books SET price=ROUND(price*1.1,2) WHERE published_year<2000;
 
 -- problem:8️⃣ (delete customer who has not placed any orders)
-
 DELETE FROM customers 
 WHERE id IN (SELECT customers.id FROM orders 
 RIGHT JOIN customers ON orders.customer_id=customers.id
